de063060e63d9fbc1d0c026fa606fa58bbb56101
Zihong <Zihongchen0227@gmail.com>
2026-08-29 20:12:40 -0400

    close to done with frontend

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState, useLayoutEffect } from "react";
+import gsap from "gsap/all";
 
 const CODE_LINES = 27;
 
@@ -97,4 +98,107 @@ const Resources = () => {
   );
 };
 
-export default Resources;
+const Folder = ({ folderFront, folderBack }) => {
+  const baseHeight = useRef(null);
+  const isOpen = useRef(false);
+
+  const open = (el) => {
+    if (baseHeight.current === null) baseHeight.current = el.offsetHeight;
+    gsap.to(el, { height: baseHeight.current * 2, duration: 0.6 });
+    gsap.to(el.querySelector(".backFolder"), {
+      rotationX: 180,
+      transformOrigin: "bottom center",
+      duration: 0.6,
+    });
+    isOpen.current = true;
+  };
+
+  const close = (el) => {
+    gsap.to(el, { height: baseHeight.current, duration: 0.6 });
+    gsap.to(el.querySelector(".backFolder"), {
+      rotationX: 0,
+      transformOrigin: "bottom center",
+      duration: 0.6,
+    });
+    isOpen.current = false;
+  };
+
+  return (
+    <div
+      onClick={(e) => {
+        const el = e.currentTarget;
+        isOpen.current ? close(el) : open(el);
+      }}
+      onMouseLeave={(e) => {
+        if (isOpen.current) close(e.currentTarget);
+      }}
+    >
+      <div className="wrapper relative">
+        <div
+          className="w-[70%] mx-auto aspect-[1315/1001] bg-no-repeat bg-[length:100%_100%]"
+          style={{ backgroundImage: `url(${folderBack})` }}
+        ></div>
+        <div
+          className="backFolder absolute inset-x-0 z-10 bottom-0 w-[70%] mx-auto aspect-[1315/886] bg-no-repeat bg-[length:100%_100%]"
+          style={{ backgroundImage: `url(${folderFront})` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+const TAB_REVEAL = 150; // px
+
+const ResourceFolders = () => {
+  const firstWrapperRef = useRef(null);
+  const [folderHeight, setFolderHeight] = useState(null);
+
+  useLayoutEffect(() => {
+    const wrapper = firstWrapperRef.current?.querySelector(".wrapper");
+    if (!wrapper) return;
+    const ro = new ResizeObserver(([entry]) => {
+      setFolderHeight(entry.contentRect.height);
+    });
+    ro.observe(wrapper);
+    return () => ro.disconnect();
+  }, []);
+
+  const pullUp = folderHeight === null ? 0 : -(folderHeight - TAB_REVEAL);
+
+  return (
+    <div className="bg-[#1C1512] p-12">
+      <div className="relative">
+        <div ref={firstWrapperRef} className="relative z-0">
+          <Folder
+            folderFront="/FolderFront4.png"
+            folderBack="/FolderBack4.png"
+          />
+        </div>
+        <div className="relative z-10" style={{ marginTop: pullUp }}>
+          <Folder
+            folderFront="/FolderFront3.png"
+            folderBack="/FolderBack3.png"
+          />
+        </div>
+        <div className="relative z-20" style={{ marginTop: pullUp }}>
+          <Folder
+            folderFront="/FolderFront2.png"
+            folderBack="/FolderBack2.png"
+          />
+        </div>
+        <div className="relative z-30" style={{ marginTop: pullUp }}>
+          <Folder folderFront="/FolderFront.png" folderBack="/FolderBack.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ResourcesPage = () => (
+  <>
+    <Resources />
+    <ResourceFolders />
+  </>
+);
+
+export default ResourcesPage;
